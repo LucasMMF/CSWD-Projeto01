9beb9f54fb1ea25c1ac491a7cf2fd8f31a92e264
LucasMMF <mateusmidao@gmail.com>
2024-02-04 19:51:34 -0300

    feat: aula01 projeto01 part 02
    the second feature of this class, in it, we create
    the 'cliente' entity and use it on Program.cs
    to store attributes of a client.
    
    BREAKING CHANGE: this feature will remove the
    feature worked on the last commit, changing the
    functionality of Program.cs from just showing
    a simple text into actually storing values in a
    class and showing those stored values on DOS.

---
+++ Projeto01/Entities/Cliente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto01.Entities
+{
+    // Definição da classe
+    public class Cliente
+    {
+        // Propriedades
+        public Guid Id { get; set; }
+        public string? Nome { get; set; }
+        public string? Email { get; set; }
+        public string? Cpf { get; set; }
+        public DateTime DataNascimento { get; set; }
+    }
+}
--- Projeto01/Program.cs
+++ Projeto01/Program.cs
@@ -1,4 +1,6 @@
-﻿// Definindo o namespace da classe (localização)
+﻿using Projeto01.Entities;
+
+// Definindo o namespace da classe (localização)
 namespace Projeto01
 {
     // Declaração da classe
@@ -8,7 +10,26 @@ namespace Projeto01
         public static void Main(string[] args)
         {
             // Imprimir mensagem (Console do DOS)
-            Console.WriteLine("Aula 01 - C# WebDeveloper");
+            Console.WriteLine("\n *** CADASTRO DE CLIENTES *** \n");
+
+            // Criando um objeto da classe Cliente (variável de instância)
+            var cliente = new Cliente();
+
+            // Preenchendo os dados do cliente:
+            cliente.Id = Guid.NewGuid();
+            cliente.Nome = "Lucas Mateus Midão Fernandes";
+            cliente.Email = "mateusmidao@gmail.com";
+            cliente.Cpf = "123.456.789-00";
+            cliente.DataNascimento = new DateTime(1996, 1, 21);
+
+            // Imprimindo os campos
+            // cw -> 2x[tab]
+            Console.WriteLine("\nDADOS DO CLIENTE:");
+            Console.WriteLine($"Id do cliente.....: {cliente.Id}");
+            Console.WriteLine($"Nome..............: {cliente.Nome}");
+            Console.WriteLine($"Email.............: {cliente.Email}");
+            Console.WriteLine($"Cpf...............: {cliente.Cpf}");
+            Console.WriteLine($"Data de Nascimento: {cliente.DataNascimento}");
 
             // Pausar o prompt de comandos
             Console.ReadKey();
