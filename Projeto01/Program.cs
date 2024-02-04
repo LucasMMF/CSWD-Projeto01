180f207da78c88507c9148d3cc2259221c5ee115
LucasMMF <mateusmidao@gmail.com>
2024-02-04 19:31:48 -0300

    feat: aula01 projeto01 part 01
    the first feature of this class, in it, we explore
    the simple structure of a C# main method.
    
    disclaimer: this is being done for learning
    purposes, with .NET 6 and beyond, Microsoft has
    adopted the concept of 'low code', meaning that
    the namespace, class and main parts of Program.cs
    don't need to be written out for the project to
    compile.

--- Projeto01/Program.cs
+++ Projeto01/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Definindo o namespace da classe (localização)
+namespace Projeto01
+{
+    // Declaração da classe
+    public class Program
+    {
+        // Método para execução / inicialização do projeto
+        public static void Main(string[] args)
+        {
+            // Imprimir mensagem (Console do DOS)
+            Console.WriteLine("Aula 01 - C# WebDeveloper");
+
+            // Pausar o prompt de comandos
+            Console.ReadKey();
+        }
+    }
+}
