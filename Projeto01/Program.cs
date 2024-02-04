54c37faaef54a86948773f51ab04e2fd664d6d64
LucasMMF <mateusmidao@gmail.com>
2024-02-04 20:12:58 -0300

    feat: aula01 projeto01 part 03
    alters the way the 'cliente' object attributes
    receive values, from hard coded values to user
    input values.

--- Projeto01/Program.cs
+++ Projeto01/Program.cs
@@ -15,21 +15,37 @@ namespace Projeto01
             // Criando um objeto da classe Cliente (variável de instância)
             var cliente = new Cliente();
 
-            // Preenchendo os dados do cliente:
-            cliente.Id = Guid.NewGuid();
-            cliente.Nome = "Lucas Mateus Midão Fernandes";
-            cliente.Email = "mateusmidao@gmail.com";
-            cliente.Cpf = "123.456.789-00";
-            cliente.DataNascimento = new DateTime(1996, 1, 21);
+            try // Tentativa
+            {
+                // Preenchendo os dados do cliente:
+                cliente.Id = Guid.NewGuid();
 
-            // Imprimindo os campos
-            // cw -> 2x[tab]
-            Console.WriteLine("\nDADOS DO CLIENTE:");
-            Console.WriteLine($"Id do cliente.....: {cliente.Id}");
-            Console.WriteLine($"Nome..............: {cliente.Nome}");
-            Console.WriteLine($"Email.............: {cliente.Email}");
-            Console.WriteLine($"Cpf...............: {cliente.Cpf}");
-            Console.WriteLine($"Data de Nascimento: {cliente.DataNascimento}");
+                Console.Write("Informe o nome do cliente.....: ");
+                cliente.Nome = Console.ReadLine();
+
+                Console.Write("Informe o email do cliente....: ");
+                cliente.Email = Console.ReadLine();
+
+                Console.Write("Informe o cpf do cliente......: ");
+                cliente.Cpf = Console.ReadLine();
+
+                Console.Write("Informe a data de nascimento..: ");
+                cliente.DataNascimento = DateTime.Parse(Console.ReadLine());
+
+                // Imprimindo os campos
+                // cw -> 2x[tab]
+                Console.WriteLine("\nDADOS DO CLIENTE:");
+                Console.WriteLine($"Id do cliente.....: {cliente.Id}");
+                Console.WriteLine($"Nome..............: {cliente.Nome}");
+                Console.WriteLine($"Email.............: {cliente.Email}");
+                Console.WriteLine($"Cpf...............: {cliente.Cpf}");
+                Console.WriteLine($"Data de Nascimento: {cliente.DataNascimento}");
+            }
+            catch ( Exception e ) // Capturar a exceção (erro)
+            {
+                Console.WriteLine("\n Falha ao cadastrar o cliente! \n");
+                Console.WriteLine($"Erro: {e.Message}");
+            }
 
             // Pausar o prompt de comandos
             Console.ReadKey();
